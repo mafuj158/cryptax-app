e6e2bd7377836231e621f4c42c03c57e30c3b19d
bishal158 <afnanmafuj22@gmail.com>
2026-05-07 21:56:27 +0600

    Refactor icon components to use react-native-svg and add focused state
    - Updated CrypstoreIcon and NewsIcon components to accept a focused prop for dynamic coloring.
    - Converted SVG elements to use react-native-svg for better compatibility with React Native.
    - Added new HomeIcon, MessagesIcon, PodcastIcon, ProfileIcon, SubscriptionsIcon, and TaxAdvisorIcon components with SVG paths.
    - Ensured all new icons are exported for use in the application.

--- app/cryptax/(tabs)/_layout.tsx
+++ app/cryptax/(tabs)/_layout.tsx
@@ -1,70 +1,62 @@
+import AiIcon from "@/components/icons/ai"
+import CrypstoreIcon from "@/components/icons/crypstore"
+import NewsIcon from "@/components/icons/news"
+
 import { Tabs } from "expo-router"
-import { Image, View } from "react-native"
+import { View } from "react-native"
+
+type TabRoute = {
+    name: string
+    title: string
+    icon: React.ComponentType<{ focused: boolean }>
+}
 
 const TabLayout = () => {
 
-    const tabRoutes = [
+    const tabRoutes: TabRoute[] = [
         {
             name: "index",
             title: "News",
-            focusedIcon: require("@/assets/icons/focused-news.png"),
-            unfocusedIcon: require("@/assets/icons/unfocused-news.png")
+            icon: NewsIcon,
         },
         {
             name: "ai-search",
             title: "AI Search",
-            focusedIcon: require("@/assets/icons/focused-ai.png"),
-            unfocusedIcon: require("@/assets/icons/unfocused-ai.png")
+            icon: AiIcon,
         },
         {
             name: "crypstore",
             title: "Crypstore",
-            focusedIcon: require("@/assets/icons/focused-ai.png"),
-            unfocusedIcon: require("@/assets/icons/unfocused-ai.png")
-        },
-        {
-            name: "more",
-            title: "More",
-            focusedIcon: require("@/assets/icons/focused-news.png"),
-            unfocusedIcon: require("@/assets/icons/unfocused-news.png")
-        },
-        {
-            name: "setting",
-            title: "Setting",
-            focusedIcon: require("@/assets/icons/focused-news.png"),
-            unfocusedIcon: require("@/assets/icons/unfocused-news.png")
+            icon: CrypstoreIcon,
         },
     ]
 
     const renderTabs = () => {
-        return tabRoutes.map((route) => (
-            <Tabs.Screen
-                key={route.name}
-                name={route.name}
-                options={{
-                    title: route.title,
-                    tabBarIcon: ({ focused }) => (
-                        <View className="flex size-6 justify-center items-center">
-                            <Image
-                                source={focused ? route.focusedIcon : route.unfocusedIcon}
-                                className="w-full h-full object-contain"
-                            />
-                        </View>
-                    )
-                }}
-            />
-        ))
+        return tabRoutes.map((route) => {
+            const IconComponent = route.icon
+            return (
+                <Tabs.Screen
+                    key={route.name}
+                    name={route.name}
+                    options={{
+                        title: route.title,
+                        tabBarIcon: ({ focused }) => (
+                            <View className="flex size-6 justify-center items-center">
+                                <IconComponent focused={focused} />
+                            </View>
+                        )
+                    }}
+                />
+            )
+        })
     }
 
-
-    // main render
     return (
         <Tabs screenOptions={{
             headerShown: false,
             tabBarActiveTintColor: '#2b7fff',
             tabBarInactiveTintColor: '#373737',
             tabBarStyle: {
-
                 backgroundColor: '#ffffff',
                 shadowColor: '#000',
                 height: 70,
@@ -86,4 +78,4 @@ const TabLayout = () => {
     )
 }
 
-export default TabLayout;
+export default TabLayout
---
+++ components/icons/subscriptions.tsx
@@ -0,0 +1,13 @@
+const SubscriptionsIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path fillRule="evenodd" clipRule="evenodd" d="M5 4C3.89543 4 3 4.89543 3 6V18C3 19.1046 3.89543 20 5 20H19C20.1046 20 21 19.1046 21 18V6C21 4.89543 20.1046 4 19 4H5ZM1 6C1 3.79086 2.79086 2 5 2H19C21.2091 2 23 3.79086 23 6V18C23 20.2091 21.2091 22 19 22H5C2.79086 22 1 20.2091 1 18V6Z" fill="#373737"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M7 8.5C7 8.22386 7.22386 8 7.5 8H16.5C16.7761 8 17 8.22386 17 8.5C17 8.77614 16.7761 9 16.5 9H7.5C7.22386 9 7 8.77614 7 8.5Z" fill="#373737"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M7 12.5C7 12.2239 7.22386 12 7.5 12H16.5C16.7761 12 17 12.2239 17 12.5C17 12.7761 16.7761 13 16.5 13H7.5C7.22386 13 7 12.7761 7 12.5Z" fill="#373737"/>
+            <path d="M15 17C15 15.8954 15.8954 15 17 15H18C19.1046 15 20 15.8954 20 17V18C20 19.1046 19.1046 20 18 20H17C15.8954 20 15 19.1046 15 18V17Z" fill="#373737"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M11.5 2C11.2239 2 11 2.22386 11 2.5V4H13V2.5C13 2.22386 12.7761 2 12.5 2H11.5Z" fill="#373737"/>
+        </svg>
+    )
+}
+
+export default SubscriptionsIcon
